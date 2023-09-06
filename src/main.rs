48bfdfb4c3b56974a4bd200f0e7989fbfcd2162b
Growthack90 <hackmaker90@outlook.it>
2023-09-06 15:14:52 +0200

    minor fix

--- src/main.rs
+++ src/main.rs
@@ -62,7 +62,7 @@ fn main() {
         };
 
         // Stampa il risultato
-        println!("Risultato {} {} --> : {:.2} {}", input_value, input_unit, result, output_unit);
+        print!("Risultato:\n {} ({}) --> {:.2} ({})\n", input_value, input_unit.trim(), result, output_unit.trim());
 
 
         // Chiedi all'utente se desidera eseguire un'altra conversione
